2f672a673bb7508c69775895ba2b433cb8984506
abbnilbor <43879825+nilbor@users.noreply.github.com>
2019-03-12 16:22:18 +0100

    ändra yes

--- Kapitel 5/5.2.py
+++ Kapitel 5/5.2.py
@@ -1,5 +1,14 @@
 a=input("Ange ditt namn: ")
-b=input("Ange din ålder: ")
+b=input("Ange din ålder: ") #två prompts
 
-ageArray = range(18)
-sleepArray= [14,13,12,11.5,11,10.5,9.5,9,8.5,8]
+sleepArray=[14,13,12,11.5,11,11,10.5,10,10,10,9.5,9,9,9,9,8.5]
+ageArray=[1,2,3,4,5,6, 7, 8, 9, 10, 11, 12, 14, 15, 16] #arrays med värden
+
+for x in ageArray:
+    if x==int(b): #för varje värde mellan 1-16 så kollar den om det är samma sak som b
+        c=sleepArray[x-1] #då hittar den motsvarande positioion i sleepArray och gör c till värdet
+
+if int(b)>=17:
+    c=8 #utifall värdet skulle vara 17 eller över så blir c 8
+    
+print("Hallå " + a+ "! Enligt Vårdguidens rekommendationer behöver individer i din ålder (" + b + " år) sova minst " + str(c) + " timmar per natt." )
