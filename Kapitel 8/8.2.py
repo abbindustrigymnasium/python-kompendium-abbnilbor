98520e96da355cd07f2aedada7481c0c771f79fa
abbnilbor <43879825+nilbor@users.noreply.github.com>
2019-04-12 14:31:28 +0200

    glömde kommentera

--- Kapitel 8/8.2.py
+++ Kapitel 8/8.2.py
@@ -1,5 +1,5 @@
-import web
+import web #importerar filen med get funktionen
 
-a=""
-a=web.get("https://5hyqtreww2.execute-api.eu-north-1.amazonaws.com/artists/") #exempelurl för uppgift 6.3
-print(a)
+a="" #den ville ha a som någonting
+a=web.get("https://5hyqtreww2.execute-api.eu-north-1.amazonaws.com/artists/") #exempelurl för uppgift 6.3, get returnerar ett värde som vi sätter a till
+print(a) 
